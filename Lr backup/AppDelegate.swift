be039c704a37cb5f1e4e25937b140c16e0d2c241
GaffelGaffel <k.julmust@gmail.com>
2015-08-08 13:15:02 +0200

    Initial Commit

---
+++ Lr backup/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Lr backup
+//
+//  Created by Karl Persson on 2015-08-08.
+//  Copyright (c) 2015 Karl Persson. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+	@IBOutlet weak var window: NSWindow!
+
+
+	func applicationDidFinishLaunching(aNotification: NSNotification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(aNotification: NSNotification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
